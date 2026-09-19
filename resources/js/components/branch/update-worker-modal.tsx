8a8ecf8ef0461fdf97ffc519ecfb43ae19519b3f
Islombek Fargoniy <iosdevelopmentcenter@iOSs-MacBook.local>
2026-09-19 18:01:58 +0500

    fix: add missing useState import in update-worker-modal

--- resources/js/components/branch/update-worker-modal.tsx
+++ resources/js/components/branch/update-worker-modal.tsx
@@ -1,5 +1,5 @@
 import { useForm } from '@inertiajs/react';
-import { FormEventHandler, useEffect, useRef } from 'react';
+import { FormEventHandler, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import InputError from '@/components/input-error';
