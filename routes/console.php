04a2dc2f83aee4c53a495f33c7068e33ec6c7c4f
Islombek Fargoniy <iosdevelopmentcenter@iOSs-MacBook.local>
2026-09-19 16:25:10 +0500

    Update schedule frequency to everyMinute with background execution

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-\Illuminate\Support\Facades\Schedule::command('hikvision:sync-events')->everyFiveMinutes();
+Schedule::command('hikvision:sync-events')->everyMinute()->runInBackground();
 
