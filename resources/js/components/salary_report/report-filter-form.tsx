8fc1340f82769bfab83c5f45ceafb42b2abb805e
Islombek Fargoniy <iosdevelopmentcenter@iOSs-MacBook.local>
2026-09-20 17:53:56 +0500

    Deploy updates 2026-09-20 17:53:55

--- resources/js/components/salary_report/report-filter-form.tsx
+++ resources/js/components/salary_report/report-filter-form.tsx
@@ -92,7 +92,7 @@ const ReportFilterForm = ({
                             dateFormat="yyyy-MM-dd"
                             placeholderText={`${t('from')} — ${t('to')}`}
                             wrapperClassName="w-full sm:w-auto"
-                            className="h-9 w-full sm:w-[180px] rounded-xl border border-slate-200 bg-white pl-2.5 pr-5.5 py-1.5 text-xs font-medium text-slate-700 shadow-2xs transition-colors hover:bg-slate-50 focus:border-indigo-500 focus:outline-hidden focus:ring-2 focus:ring-indigo-500/20 dark:border-slate-800 dark:bg-slate-900 dark:text-slate-200 dark:hover:bg-slate-800 cursor-pointer"
+                            className="h-9 w-full sm:w-[192px] rounded-xl border border-slate-200 bg-white pl-2.5 pr-5.5 py-1.5 text-xs font-medium text-slate-700 shadow-2xs transition-colors hover:bg-slate-50 focus:border-indigo-500 focus:outline-hidden focus:ring-2 focus:ring-indigo-500/20 dark:border-slate-800 dark:bg-slate-900 dark:text-slate-200 dark:hover:bg-slate-800 cursor-pointer"
                         />
                     </div>
                 )}
