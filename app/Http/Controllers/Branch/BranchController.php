495c1917a934b98e33249d1dd6ab68c491752fd3
Islombek Fargoniy <iosdevelopmentcenter@iOSs-MacBook.local>
2026-09-21 11:26:13 +0500

    Fix DB import in BranchController

--- app/Http/Controllers/Branch/BranchController.php
+++ app/Http/Controllers/Branch/BranchController.php
@@ -11,6 +11,7 @@ use App\Models\Firm\Firm;
 use App\Models\Worker\Worker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
